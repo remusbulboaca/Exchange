b90efc4a20f5c316bd923bb73afb45df88e2837b
remusbulboaca <47847935+remusbulboaca@users.noreply.github.com>
2020-11-30 09:12:58 +0200

    GUI update

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import requests
 import bs4
 from tkinter import *
 
-def getDataFromBNR():
+
+def getdatafrombnr():
     # Request catre BNR pentru pagina de curs valutar
     res = requests.get('https://www.bnr.ro/Cursul-de-schimb-524.aspx')
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
@@ -15,62 +16,72 @@ def getDataFromBNR():
         data[cols[1].text] = cols[6].text.replace(',', '.')
     return data
 
-def getAllCurency():
+
+def get_all_curency():
     lista = list()
-    data = getDataFromBNR()
+    data = getdatafrombnr()
     for x in data.keys():
         lista.append(x)
     return lista
 
+
 def exchange(currency):
-    data = getDataFromBNR()
+    data = getdatafrombnr()
     return data[currency]
 
-def fromCurrencyToCurrency(value,currency1, currency2):
-    firstCurrency = float(exchange(currency1))
-    secondCurrency = float(exchange(currency2))
-    rate = firstCurrency / secondCurrency
-    moneyReceived = value * rate
-    return float(moneyReceived)
+
+def from_curency_to_curency(value, currency1, currency2):
+    first_currency = float(exchange(currency1))
+    second_currency = float(exchange(currency2))
+    rate = first_currency / second_currency
+    money_received = value * rate
+    return float(money_received)
+
 
 def click(selection):
     print(selection)
 
-def dropdownClicked(selection):
-    global fCurency
-    fCurency = selection
-    print(fCurency)
 
-def dropdown2Clicked(selection):
-    global sCurency
-    sCurency = selection
-    print(sCurency)
+def dropdown_clicked(selection):
+    global f_curency
+    f_curency = selection
+    print(f_curency)
+
+
+def dropdown2_clicked(selection):
+    global s_curency
+    s_curency = selection
+    print(s_curency)
+
 
 def test():
-    print(fCurency)
-    print(sCurency)
+    firstCurrency = float(exchange(f_curency))
+    secondCurrency = float(exchange(s_curency))
+    rate = firstCurrency / secondCurrency
+    print(rate * float(e.get()))
 
-global fCurency
-global sCurency
 
-firstValue = getAllCurency()
+global f_curency
+global s_curency
+
+firstValue = get_all_curency()
 program = Tk()
 program.title("Schimb valutar")
-program.minsize(700,400)
-program.resizable(None,None)
-program.maxsize(701,401)
-label1 = Label(program,text="Introdu suma:",pady=15)
+program.minsize(700, 400)
+program.resizable(None, None)
+program.maxsize(701, 401)
+label1 = Label(program, text="Introdu suma:", pady=15)
 label1.pack()
 e = Entry(program, width=25)
 e.pack()
 clicked = StringVar()
-dropdown1 = OptionMenu(program,clicked,*getAllCurency(),command=dropdownClicked)
+dropdown1 = OptionMenu(program, clicked, *get_all_curency(), command=dropdown_clicked)
 dropdown1.pack()
-label2 = Label(program,text="Selecteaza moneda in care vrei sa convertesti",pady=7)
+label2 = Label(program, text="Selecteaza moneda in care vrei sa convertesti", pady=7)
 label2.pack()
 clicked2 = StringVar()
-dropdown2 = OptionMenu(program,clicked2,*getAllCurency(),command=dropdown2Clicked)
+dropdown2 = OptionMenu(program, clicked2, *get_all_curency(), command=dropdown2_clicked)
 dropdown2.pack()
-myButton = Button(program,text="Converteste",padx=30,pady=15, fg="green",command=test)
+myButton = Button(program, text="Converteste", padx=30, pady=15, fg="green", command=test)
 myButton.pack()
 program.mainloop()
